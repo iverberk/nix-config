02419a60a79c6c9e90eb66560a09126c6476fa14
Ivo Verberk <ivo@itiation.nl>
2022-07-03 12:32:41 +0200

    Update

--- nvim/lua/lsp/providers/null-ls.lua
+++ nvim/lua/lsp/providers/null-ls.lua
@@ -3,6 +3,7 @@ local null_ls = require('null-ls')
 local merge = require('utils').merge
 
 require('null-ls').setup(merge(defaults, {
+  debug = false,
   sources = {
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.code_actions.shellcheck,
@@ -12,9 +13,8 @@ require('null-ls').setup(merge(defaults, {
     null_ls.builtins.diagnostics.shellcheck.with({ diagnostics_format = "[#{c}] #{m} (#{s})" }),
     null_ls.builtins.diagnostics.yamllint.with({args = { "--format", "parsable", "-" }}),
     null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.formatting.prettier_d_slim,
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.fixjson
