c683e17aeca3b66bb644d7c729d689124666507a
Ivo Verberk <ivo@itiation.nl>
2022-07-08 08:11:39 +0200

    Fix lazygit when switching sessions

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,17 +3,25 @@ local Terminal = require('toggleterm.terminal').Terminal
 
 toggleterm.setup({})
 
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = {
-    border = "double",
-  }
-})
-
 function _lazygit_toggle()
-  lazygit:toggle()
+  local count = 0
+
+  for char in string.gmatch(vim.api.nvim_eval("sha256(getcwd())"), '%S') do
+    count = count + string.byte(char)
+  end
+
+  local term = Terminal:new({
+    count = count,
+    cmd = "lazygit",
+    dir = "git_dir",
+    direction = "float",
+    close_on_exit = true,
+    float_opts = {
+      border = "double",
+    }
+  })
+
+  term:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
