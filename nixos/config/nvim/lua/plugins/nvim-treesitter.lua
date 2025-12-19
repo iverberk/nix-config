610be79d28f6928b97b0d04fe8bca3ec41340df8
Ivo Verberk <ivo.verberk@gmail.com>
2025-12-20 00:02:47 +0100

    flake update and some small fixes

--- nixos/config/nvim/lua/plugins/nvim-treesitter.lua
+++ nixos/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-        ensure_installed = { "lua", "python", "javascript", "go", "json", "yaml", "terraform", "regex" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-  end
+  lazy = false,
+  build = ":TSUpdate"
 }
